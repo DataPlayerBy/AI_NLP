1e533984b5f03fecc373bf188893de7124712b9a
gaojingbin <gaojingbin@weiboyi.com>
2019-02-23 16:29:15 +0800

    Add overlap processing for Sentiment Analysis identifier code on  IMDB data set
    影评情感分类的过拟合处理代码

--- IMDB/__init__.py
+++ IMDB/__init__.py
@@ -1,2 +1,3 @@
 # 要先将数据拷贝到/Users/gaojingbin/.keras/datasets下
-# 参考https://tensorflow.google.cn/tutorials/keras/basic_text_classification
+# main.py参考https://tensorflow.google.cn/tutorials/keras/basic_text_classification
+# main_pros.py参考https://tensorflow.google.cn/tutorials/keras/overfit_and_underfit
